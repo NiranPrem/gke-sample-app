9923b7ac5343aa2b9c7c62e317bef028726c20ba
NiranPrem <163099269+NiranPrem@users.noreply.github.com>
2025-09-09 12:03:19 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,19 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__, static_folder="../frontend", static_url_path="/")
+app = Flask(__name__, static_folder="../frontend")
 
-@app.route("/")
-def index():
-    return send_from_directory("../frontend", "index.html")
+@app.route('/')
+def serve_index():
+    return send_from_directory(app.static_folder, "index.html")
 
-@app.route("/api/hello")
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory(app.static_folder, path)
+
+@app.route('/api/hello')
 def hello():
-    return jsonify({"message": "Hello from Flask backend 🚀 running on GKE!"})
+    return {"message": "Hello from Flask backend!"}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
